5228a075e8323593cf72d4aa43fc23af1648d33d
Nicolas Revel <nicolas.revel@laplateforme.io>
2022-03-29 18:26:01 +0200

    adding rgpd page

---
+++ pages/rgpd.js
@@ -0,0 +1,11 @@
+import { StayPrimaryLandscape } from "@mui/icons-material";
+import { Typography } from "@mui/material";
+
+export default function rgpd() {
+	return (
+		<Typography variant='h1' sx={{fontFamily: 'Montserrat', fontWeight: 600, color: 'primary.main' }}>
+			POLITIQUE DE CONFIDENTIALITÉ ET DE PROTECTION DES DONNÉES
+			PERSONNELLES
+		</Typography>
+	);
+}
