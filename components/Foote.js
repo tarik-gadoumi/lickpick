9d9431978964c7ee990b80eafc333c130d648a96
aichaouattara <aicha.ouattara@laplateforme.io>
2022-03-30 09:31:12 +0200

    footer change

---
+++ components/Foote.js
@@ -0,0 +1,24 @@
+// import React from "react";
+// import Container from "@mui/material/Container";
+
+// export default function Foote() {
+//   return (
+//     <container>
+//       <Grid item xs={12} sm={6} md={3}>
+//         <div>item</div>
+//       </Grid>
+//       <Grid item xs={12} sm={6} md={3}>
+//         <div>item</div>
+//       </Grid>{" "}
+//       <Grid item xs={12} sm={6} md={3}>
+//         <div>item</div>
+//       </Grid>{" "}
+//       <Grid item xs={12} sm={6} md={3}>
+//         <div>item</div>
+//       </Grid>{" "}
+//       <Grid item xs={12} sm={6} md={3}>
+//         <div>item</div>
+//       </Grid>
+//     </container>
+//   );
+// }
